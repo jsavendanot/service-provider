0b3c1835ceea9c6bb76dbb03325bf684a64d9383
Margad-Erdene <rmargaderdene@gmail.com>
2020-04-22 17:14:12 +1000

    suggesting service provider in progress

--- src/views/Safety/components/Contact/People/index.tsx
+++ src/views/Safety/components/Contact/People/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import { Network } from 'types/network';
 import { makeStyles } from '@material-ui/styles';
-import { Phone, Add } from '@material-ui/icons';
+import {
+  Phone,
+  Add,
+  DeleteOutline,
+  AddCircleOutline
+} from '@material-ui/icons';
 import { Button } from 'common/components';
+import { Value } from 'types/safety';
+import uuid from 'uuid';
+import { useDispatch } from 'react-redux';
+import { IconButton, TextField } from '@material-ui/core';
+import Confirmation from 'common/components/Confirmation';
+import { suggestSafetyPlan } from 'slices/suggestion/action';
 
 const useStyles = makeStyles(() => ({
   descText: {
@@ -51,6 +62,53 @@ const useStyles = makeStyles(() => ({
     lineHeight: '23px',
     color: '#C57D7D',
     marginRight: '5px'
+  },
+  suggestedRowContainer: {
+    width: '100%',
+    display: 'flex'
+  },
+  suggestedRow: {
+    width: '85%',
+    padding: '10px',
+    background: '#FFFAE9',
+    borderRadius: '4px',
+    marginBottom: '20px',
+    wordWrap: 'break-word'
+  },
+  row: {
+    width: '85%',
+    padding: '10px',
+    background: '#FFFAE9',
+    borderRadius: '4px',
+    marginBottom: '20px'
+  },
+  valueText: {
+    fontFamily: 'Scada',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '18px',
+    lineHeight: '22px',
+    color: '#323F45'
+  },
+  textFieldContainer: {
+    padding: '10px 0',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  textField: {
+    width: '85%',
+    background: '#FFFAE9',
+    marginRight: '10px'
+  },
+  title: {
+    fontFamily: 'Scada',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    fontSize: '20px',
+    lineHeight: '25px',
+    color: '#692B40',
+    margin: '10px 0',
+    flexGrow: 1
   }
 }));
 
@@ -60,8 +118,51 @@ type Props = {
 
 export const People: React.FC<Props> = ({ people }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const [suggestedValues, setSuggestedValues] = useState<Value[]>([]);
+  const [addClicked, setAddClicked] = useState(false);
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.persist();
+    setInput(event.target.value);
+  };
+
+  const addToSuggestedValues = () => {
+    if (input.length > 4) {
+      setSuggestedValues(values => [
+        ...values,
+        {
+          id: uuid(),
+          name: input
+        }
+      ]);
+      dispatch(suggestSafetyPlan(input, 'Services', 'person'));
+      setInput('');
+    }
+  };
+
+  const removeFromSuggestedValues = (id: string) => {
+    const updatedSuggestedStr = suggestedValues.filter(item => item.id !== id);
+    setSuggestedValues(updatedSuggestedStr);
+  };
+
+  /** Dialog */
+  const [open, setOpen] = useState(false);
+
+  function openDialog() {
+    if (input.length > 4) {
+      setOpen(true);
+    }
+  }
+
+  function closeDialog() {
+    setOpen(false);
+  }
+
   return (
-    <div>
+    <>
       <span className={classes.descText}>
         People or services who I can contact for support if I need immediate
         help.
@@ -80,9 +181,54 @@ export const People: React.FC<Props> = ({ people }) => {
           </div>
         );
       })}
+      {suggestedValues.map(value => {
+        return (
+          <div key={value.id} className={classes.suggestedRowContainer}>
+            <div className={classes.suggestedRow}>
+              <span className={classes.valueText}>{value.name}</span>
+            </div>
+            <div style={{ height: '30px' }}>
+              <IconButton
+                onClick={() => removeFromSuggestedValues(value.id)}
+                style={{ padding: '5px', marginLeft: '11px' }}>
+                <DeleteOutline
+                  style={{
+                    fill: '#C57D7D'
+                  }}
+                  fontSize="large"
+                />
+              </IconButton>
+            </div>
+          </div>
+        );
+      })}
+      {addClicked && (
+        <div className={classes.textFieldContainer}>
+          <TextField
+            fullWidth
+            label="Type here..."
+            name="input"
+            autoComplete="off"
+            multiline
+            value={input}
+            variant="outlined"
+            className={classes.textField}
+            onChange={handleInputChange}
+            inputProps={{ maxLength: 500 }}
+          />
+          <div style={{ width: '50px' }}>
+            <IconButton onClick={openDialog} style={{ padding: '5px' }}>
+              <AddCircleOutline
+                style={{ fill: '#C57D7D', cursor: 'pointer' }}
+                fontSize="large"
+              />
+            </IconButton>
+          </div>
+        </div>
+      )}
       <div className={classes.action}>
         <div style={{ width: '91px', marginRight: '20px' }}>
-          <Button type="primarySmall">
+          <Button type="primarySmall" click={() => setAddClicked(true)}>
             <Add style={{ marginRight: '5px' }} />
             Add
           </Button>
@@ -98,7 +244,20 @@ export const People: React.FC<Props> = ({ people }) => {
           </Button>
         </div>
       </div>
-    </div>
+      {open && (
+        <Confirmation
+          open={open}
+          close={closeDialog}
+          action={addToSuggestedValues}
+          donRedirect>
+          <span className={classes.title}>
+            Are you sure you want to
+            <br />
+            suggest this contact?
+          </span>
+        </Confirmation>
+      )}
+    </>
   );
 };
 
--- src/views/Safety/components/Contact/Services/index.tsx
+++ src/views/Safety/components/Contact/Services/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import { Network } from 'types/network';
 import { makeStyles } from '@material-ui/styles';
-import { Phone, Add } from '@material-ui/icons';
+import {
+  Phone,
+  Add,
+  DeleteOutline,
+  AddCircleOutline
+} from '@material-ui/icons';
 import { Button } from 'common/components';
+import { useDispatch } from 'react-redux';
+import { Value } from 'types/safety';
+import uuid from 'uuid';
+import { suggestSafetyPlan } from 'slices/suggestion/action';
+import { IconButton, TextField } from '@material-ui/core';
+import Confirmation from 'common/components/Confirmation';
 
 const useStyles = makeStyles(() => ({
   values: {
@@ -42,6 +53,53 @@ const useStyles = makeStyles(() => ({
     lineHeight: '23px',
     color: '#C57D7D',
     marginRight: '5px'
+  },
+  suggestedRowContainer: {
+    width: '100%',
+    display: 'flex'
+  },
+  suggestedRow: {
+    width: '85%',
+    padding: '10px',
+    background: '#FFFAE9',
+    borderRadius: '4px',
+    marginBottom: '20px',
+    wordWrap: 'break-word'
+  },
+  row: {
+    width: '85%',
+    padding: '10px',
+    background: '#FFFAE9',
+    borderRadius: '4px',
+    marginBottom: '20px'
+  },
+  valueText: {
+    fontFamily: 'Scada',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '18px',
+    lineHeight: '22px',
+    color: '#323F45'
+  },
+  textFieldContainer: {
+    padding: '10px 0',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  textField: {
+    width: '85%',
+    background: '#FFFAE9',
+    marginRight: '10px'
+  },
+  title: {
+    fontFamily: 'Scada',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    fontSize: '20px',
+    lineHeight: '25px',
+    color: '#692B40',
+    margin: '10px 0',
+    flexGrow: 1
   }
 }));
 
@@ -52,6 +110,49 @@ type Props = {
 
 export const Services: React.FC<Props> = ({ services, collapse }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const [suggestedValues, setSuggestedValues] = useState<Value[]>([]);
+  const [addClicked, setAddClicked] = useState(false);
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.persist();
+    setInput(event.target.value);
+  };
+
+  const addToSuggestedValues = () => {
+    if (input.length > 4) {
+      setSuggestedValues(values => [
+        ...values,
+        {
+          id: uuid(),
+          name: input
+        }
+      ]);
+      dispatch(suggestSafetyPlan(input, 'Services', 'organisation'));
+      setInput('');
+    }
+  };
+
+  const removeFromSuggestedValues = (id: string) => {
+    const updatedSuggestedStr = suggestedValues.filter(item => item.id !== id);
+    setSuggestedValues(updatedSuggestedStr);
+  };
+
+  /** Dialog */
+  const [open, setOpen] = useState(false);
+
+  function openDialog() {
+    if (input.length > 4) {
+      setOpen(true);
+    }
+  }
+
+  function closeDialog() {
+    setOpen(false);
+  }
+
   return (
     <div style={{ margin: '5px 0' }}>
       {collapse && (
@@ -72,9 +173,54 @@ export const Services: React.FC<Props> = ({ services, collapse }) => {
               </div>
             );
           })}
+          {suggestedValues.map(value => {
+            return (
+              <div key={value.id} className={classes.suggestedRowContainer}>
+                <div className={classes.suggestedRow}>
+                  <span className={classes.valueText}>{value.name}</span>
+                </div>
+                <div style={{ height: '30px' }}>
+                  <IconButton
+                    onClick={() => removeFromSuggestedValues(value.id)}
+                    style={{ padding: '5px', marginLeft: '11px' }}>
+                    <DeleteOutline
+                      style={{
+                        fill: '#C57D7D'
+                      }}
+                      fontSize="large"
+                    />
+                  </IconButton>
+                </div>
+              </div>
+            );
+          })}
+          {addClicked && (
+            <div className={classes.textFieldContainer}>
+              <TextField
+                fullWidth
+                label="Type here..."
+                name="input"
+                autoComplete="off"
+                multiline
+                value={input}
+                variant="outlined"
+                className={classes.textField}
+                onChange={handleInputChange}
+                inputProps={{ maxLength: 500 }}
+              />
+              <div style={{ width: '50px' }}>
+                <IconButton onClick={openDialog} style={{ padding: '5px' }}>
+                  <AddCircleOutline
+                    style={{ fill: '#C57D7D', cursor: 'pointer' }}
+                    fontSize="large"
+                  />
+                </IconButton>
+              </div>
+            </div>
+          )}
           <div className={classes.action}>
             <div style={{ width: '91px', marginRight: '20px' }}>
-              <Button type="primarySmall">
+              <Button type="primarySmall" click={() => setAddClicked(true)}>
                 <Add style={{ marginRight: '5px' }} />
                 Add
               </Button>
@@ -91,6 +237,19 @@ export const Services: React.FC<Props> = ({ services, collapse }) => {
             </div>
           </div>
         </div>
+      )}
+      {open && (
+        <Confirmation
+          open={open}
+          close={closeDialog}
+          action={addToSuggestedValues}
+          donRedirect>
+          <span className={classes.title}>
+            Are you sure you want to
+            <br />
+            suggest this contact?
+          </span>
+        </Confirmation>
       )}
     </div>
   );
