290ab037aaca34cc92d344fbc4debd92f7bc993a
Margad-Erdene <rmargaderdene@gmail.com>
2020-04-22 16:36:57 +1000

    suggesting unwell in progress

--- src/views/Safety/components/Unwell/PleaseDo/index.tsx
+++ src/views/Safety/components/Unwell/PleaseDo/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Unwell } from 'types/safety';
-import { CheckCircle, People, Add } from '@material-ui/icons';
+import React, { useState, ChangeEvent } from 'react';
+import { Unwell, Value } from 'types/safety';
+import {
+  CheckCircle,
+  People,
+  Add,
+  DeleteOutline,
+  AddCircleOutline
+} from '@material-ui/icons';
 import { makeStyles } from '@material-ui/styles';
 
 import { Button } from 'common/components';
 import { useSelector } from 'react-redux';
 import { RootState } from 'reducer';
 import { Network } from 'types/network';
+import uuid from 'uuid';
+import { IconButton, TextField } from '@material-ui/core';
+import Confirmation from 'common/components/Confirmation';
 
 const useStyles = makeStyles(() => ({
   descText: {
@@ -19,8 +28,7 @@ const useStyles = makeStyles(() => ({
     margin: '10px 0'
   },
   values: {
-    display: 'flex',
-    flexDirection: 'column'
+    width: '85%'
   },
   value: {
     padding: '10px',
@@ -56,6 +64,53 @@ const useStyles = makeStyles(() => ({
     lineHeight: '23px',
     color: '#C57D7D',
     marginRight: '5px'
+  },
+  suggestedRowContainer: {
+    width: '100%',
+    display: 'flex'
+  },
+  suggestedRow: {
+    width: '85%',
+    padding: '10px',
+    background: '#FFFAE9',
+    borderRadius: '4px',
+    marginBottom: '20px',
+    wordWrap: 'break-word'
+  },
+  row: {
+    width: '85%',
+    padding: '10px',
+    background: '#FFFAE9',
+    borderRadius: '4px',
+    marginBottom: '20px'
+  },
+  valueText: {
+    fontFamily: 'Scada',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '18px',
+    lineHeight: '22px',
+    color: '#323F45'
+  },
+  textFieldContainer: {
+    padding: '10px 0',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  textField: {
+    width: '85%',
+    background: '#FFFAE9',
+    marginRight: '10px'
+  },
+  title: {
+    fontFamily: 'Scada',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    fontSize: '20px',
+    lineHeight: '25px',
+    color: '#692B40',
+    margin: '10px 0',
+    flexGrow: 1
   }
 }));
 
@@ -70,8 +125,49 @@ export const PleaseDo: React.FC<Props> = ({ pleaseDo }) => {
     (state: RootState) => state.network.networks
   );
 
+  const [suggestedValues, setSuggestedValues] = useState<Value[]>([]);
+  const [addClicked, setAddClicked] = useState(false);
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.persist();
+    setInput(event.target.value);
+  };
+
+  const addToSuggestedValues = () => {
+    if (input.length > 4) {
+      setSuggestedValues(values => [
+        ...values,
+        {
+          id: uuid(),
+          name: input
+        }
+      ]);
+      // dispatch(suggestStayWellStressWarning(input, 'StayWell', ''));
+      setInput('');
+    }
+  };
+
+  const removeFromSuggestedValues = (id: string) => {
+    const updatedSuggestedStr = suggestedValues.filter(item => item.id !== id);
+    setSuggestedValues(updatedSuggestedStr);
+  };
+
+  /** Dialog */
+  const [open, setOpen] = useState(false);
+
+  function openDialog() {
+    if (input.length > 4) {
+      setOpen(true);
+    }
+  }
+
+  function closeDialog() {
+    setOpen(false);
+  }
+
   return (
-    <div>
+    <>
       <span className={classes.descText}>
         If I become unwell I would like these to happen or not happen.
       </span>
@@ -102,9 +198,54 @@ export const PleaseDo: React.FC<Props> = ({ pleaseDo }) => {
           </div>
         );
       })}
+      {suggestedValues.map(value => {
+        return (
+          <div key={value.id} className={classes.suggestedRowContainer}>
+            <div className={classes.suggestedRow}>
+              <span className={classes.valueText}>{value.name}</span>
+            </div>
+            <div style={{ height: '30px' }}>
+              <IconButton
+                onClick={() => removeFromSuggestedValues(value.id)}
+                style={{ padding: '5px', marginLeft: '11px' }}>
+                <DeleteOutline
+                  style={{
+                    fill: '#C57D7D'
+                  }}
+                  fontSize="large"
+                />
+              </IconButton>
+            </div>
+          </div>
+        );
+      })}
+      {addClicked && (
+        <div className={classes.textFieldContainer}>
+          <TextField
+            fullWidth
+            label="Type here..."
+            name="input"
+            autoComplete="off"
+            multiline
+            value={input}
+            variant="outlined"
+            className={classes.textField}
+            onChange={handleInputChange}
+            inputProps={{ maxLength: 500 }}
+          />
+          <div style={{ width: '50px' }}>
+            <IconButton onClick={openDialog} style={{ padding: '5px' }}>
+              <AddCircleOutline
+                style={{ fill: '#C57D7D', cursor: 'pointer' }}
+                fontSize="large"
+              />
+            </IconButton>
+          </div>
+        </div>
+      )}
       <div className={classes.action}>
         <div style={{ width: '91px', marginRight: '20px' }}>
-          <Button type="primarySmall">
+          <Button type="primarySmall" click={() => setAddClicked(true)}>
             <Add style={{ marginRight: '5px' }} />
             Add
           </Button>
@@ -120,7 +261,20 @@ export const PleaseDo: React.FC<Props> = ({ pleaseDo }) => {
           </Button>
         </div>
       </div>
-    </div>
+      {open && (
+        <Confirmation
+          open={open}
+          close={closeDialog}
+          action={addToSuggestedValues}
+          donRedirect>
+          <span className={classes.title}>
+            Are you sure you want to
+            <br />
+            suggest this unwell?
+          </span>
+        </Confirmation>
+      )}
+    </>
   );
 };
 
