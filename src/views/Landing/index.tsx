cd5a431abb0615b380458b95d6e06b59a8b1f58b
Margad-Erdene <rmargaderdene@gmail.com>
2020-06-04 00:18:39 +1000

    changed landing page UI

--- src/views/Landing/index.tsx
+++ src/views/Landing/index.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import clsx from 'clsx';
 
-import { Grid } from '@material-ui/core';
+import { Grid, Theme, IconButton, Hidden } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
-import { Login } from './components';
+import { ArrowDropDown, Menu } from '@material-ui/icons';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
   /** Header */
   header: {
     position: 'fixed',
     width: '100%',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-end',
     padding: '20px'
   },
   headerMenu: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
-    padding: '20px'
+    padding: '20px 0'
   },
   headerMenuText: {
     fontFamily: 'Roboto',
@@ -28,7 +28,18 @@ const useStyles = makeStyles(() => ({
     fontWeight: 700,
     fontSize: '18px',
     lineHeight: '127.69%',
-    color: '#C57D7D'
+    color: '#C57D7D',
+    cursor: 'pointer',
+    marginRight: '20px'
+  },
+  label: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    fontSize: '24px',
+    lineHeight: '127.69%',
+    color: '#C57D7D',
+    cursor: 'pointer'
   },
   headerButton: {
     width: '109px',
@@ -49,23 +60,108 @@ const useStyles = makeStyles(() => ({
       backgroundColor: '#FFEAEA'
     }
   },
-  /** Navigation and Title */
-  navigation: {
-    position: 'fixed',
-    left: '18%',
+  /** Image  */
+  bottonImage: {
+    position: 'absolute',
+    bottom: 0,
+    top: 'auto',
+    [theme.breakpoints.down('xs')]: {
+      width: 200,
+      height: 170,
+      marginTop: '50px'
+    },
+    [theme.breakpoints.up('xs')]: {
+      width: 250,
+      height: 220,
+      marginTop: '50px'
+    },
+    [theme.breakpoints.up('sm')]: {
+      position: 'absolute',
+      width: 300,
+      height: 270
+    },
+    [theme.breakpoints.up('md')]: {
+      width: 350,
+      height: 320
+    }
+  },
+  title: {
+    fontFamily: 'Scada',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    fontSize: '36px',
+    lineHeight: '45px',
+    color: '#37474F',
+    marginTop: '5px'
+  },
+  firstRow: {
+    marginBottom: '50px',
+    textAlign: 'center',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+      alignItems: 'center'
+    }
+  },
+  logo: {
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: '20px',
+      width: 200,
+      height: 50
+    },
+    [theme.breakpoints.up('xs')]: {
+      marginBottom: '20px'
+    },
+    [theme.breakpoints.up('md')]: {
+      marginRight: '30px'
+    }
+  },
+  row: {
+    width: '100%',
+    textAlign: 'center',
+    [theme.breakpoints.up('sm')]: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  },
+  buttonContainer: {
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'flex-end',
-    paddingTop: '7px',
-    cursor: 'pointer'
+    justifyContent: 'center'
   },
-  navText: {
-    fontFamily: 'Roboto',
-    fontStyle: 'normal',
-    fontWeight: 500,
-    fontSize: '12px',
-    lineHeight: '16px',
-    color: '#692B40'
+  signInButton: {
+    width: 151,
+    height: 59,
+    background: '#FFFFFF',
+    borderRadius: '38px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: '50px',
+    cursor: 'pointer',
+    '&:hover': {
+      background: '#AE466A',
+      boxShadow:
+        '0px 2px 4px rgba(0, 0, 0, 0.14), 0px 4px 5px rgba(0, 0, 0, 0.12), 0px 1px 10px rgba(0, 0, 0, 0.2)'
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginTop: '20px',
+      marginLeft: 0
+    }
+  },
+  arrowDown: {
+    fill: '#73BA9B'
+  },
+  middleGrid: {
+    height: '200px',
+    [theme.breakpoints.down('xs')]: {
+      height: '130px'
+    }
+  },
+  menuIcon: {
+    fill: '#C57D7D',
+    position: 'absolute',
+    top: 0
   }
 }));
 
@@ -81,24 +177,62 @@ const Landing: React.FC<Props> = ({ history }) => {
     <Grid container justify="center">
       <Grid item xs={12}>
         <div className={classes.header}>
-          <img src="/images/landing/logo.svg" alt="" />
           <div className={classes.headerMenu}>
-            <span
-              className={classes.headerMenuText}
-              style={{ marginRight: '20px' }}>
-              Help
-            </span>
-            <button
-              className={clsx(classes.headerButton, classes.headerMenuText)}
-              onClick={() => history.push('/auth')}>
-              Sign in
-            </button>
+            <Hidden mdUp>
+              <IconButton>
+                <Menu fontSize="large" className={classes.menuIcon} />
+              </IconButton>
+            </Hidden>
+            <Hidden smDown>
+              <div className={classes.headerMenuText}>
+                <span>Use Jiemba For...</span>
+                <IconButton>
+                  <ArrowDropDown className={classes.arrowDown} />
+                </IconButton>
+              </div>
+              <div className={classes.headerMenuText}>
+                <span>Information</span>
+                <IconButton>
+                  <ArrowDropDown className={classes.arrowDown} />
+                </IconButton>
+              </div>
+            </Hidden>
           </div>
         </div>
       </Grid>
-      <Grid item xs={12} style={{ height: '162px' }} />
-      <Grid item xs={12}>
-        <Login />
+      <Grid item xs={12} className={classes.middleGrid} />
+      <Grid item xs={12} container justify="center">
+        <div>
+          <div className={classes.firstRow}>
+            <img
+              src="/images/landing/logo.svg"
+              alt=""
+              className={classes.logo}
+            />
+            <div className={classes.title}>For providing services</div>
+          </div>
+          <div className={classes.row}>
+            <div
+              className={classes.label}
+              onClick={() => history.push('/auth')}>
+              Create Account
+            </div>
+            <div className={classes.buttonContainer}>
+              <div
+                className={clsx(classes.label, classes.signInButton)}
+                onClick={() => history.push('/auth')}>
+                Sign in
+              </div>
+            </div>
+          </div>
+        </div>
+      </Grid>
+      <Grid item xs={12} container justify="center">
+        <img
+          src="/images/landing/gary.svg"
+          alt=""
+          className={classes.bottonImage}
+        />
       </Grid>
     </Grid>
   );
